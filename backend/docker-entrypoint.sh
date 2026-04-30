65739352c0d99a1ef71261a6a9bf15f9a7b7592a
roland933 <mroland933@gmail.com>
2026-04-30 13:35:48 +0200

    fixing docker

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,25 +2,26 @@
 
 echo "Starting container..."
 
-# 1. Laravel install (csak ha nincs)
+# 1. Laravel install
 if [ ! -f /var/www/app/composer.json ]; then
     echo "Laravel not found, creating..."
-
     composer create-project laravel/laravel /var/www/app
-
-    php artisan key:generate
-    php artisan migrate --force
 fi
 
-# 2. Permission fix (MINDIG fusson)
-echo "Fixing permissions..."
+# 2. Composer install ha kell
+if [ ! -d /var/www/app/vendor ]; then
+    echo "Installing dependencies..."
+    composer install
+fi
 
+# 3. Permissions
 chown -R www-data:www-data /var/www/app
 chmod -R 775 /var/www/app/storage
 chmod -R 775 /var/www/app/bootstrap/cache
 chmod -R 777 /var/www/app/database
 
-# 3. (opcionális) migrate mindig
-php artisan migrate --force 
+# 4. Laravel setup
+php artisan key:generate
+php artisan migrate --force
 
 exec "$@"
